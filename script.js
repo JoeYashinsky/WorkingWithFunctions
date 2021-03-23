50d43b46d4f6e1a6ade57f62b222f21984f123fb
Your Name <joey.yashinsky@gmail.com>
2021-03-23 15:27:46 -0400

    created a function called 'createBooking' for airlines to track all potential passenger/flight bookings and details

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const booking = [];
+
+const createBooking = function(flightNumber, numberOfPassengers, price) {
+
+    const booking = {
+        flightNumber,
+        numberOfPassengers,
+        price
+    }
+    console.log(booking)
+    booking.push(booking);
+}
