1fe45972ace925cfada445cdcf2c0ba666a58d40
Your Name <joey.yashinsky@gmail.com>
2021-03-23 16:11:37 -0400

    price now set to numberOfPassengers X 199, and createBooking method utilized using 'undefined' for one of the values to  show the default value will be displayed properly

--- script.js
+++ script.js
@@ -2,9 +2,14 @@
 
 const bookings = [];
 
-const createBooking = function(flightNumber, numberOfPassengers, price) {
-    numberOfPassengers = numberOfPassengers || 1;
-    price = price || 199;
+const createBooking = function(
+    flightNumber, 
+    numberOfPassengers = 1, 
+    price = 199 * numberOfPassengers
+    ) {
+    // numberOfPassengers = numberOfPassengers || 1;
+    // price = price || 199;
+
     const booking = {
         flightNumber,
         numberOfPassengers,
@@ -16,3 +21,15 @@ const createBooking = function(flightNumber, numberOfPassengers, price) {
 
 createBooking('LH123');
 //{flightNumber: "LH123", numberOfPassengers: undefined, price: undefined}
+
+//After including OR Operator with default values
+// {flightNumber: "LH123", numberOfPassengers: 1, price: 199}
+
+createBooking('LH123', 2, 800);
+// {flightNumber: "LH123", numberOfPassengers: 2, price: 800}
+
+// After changing 'price' to 199 * numberOfPassengers
+createBooking('LH123', 2) // {flightNumber: "LH123", #ofPass: 2, price: 398}
+createBooking('LH123', 5) // {flightNumber: "LH123", #ofPass: 5, price: 995}
+
+createBooking('LH123', undefined, 1000); //{flight#: "LH123", #ofPass: 1, price: 1000}
