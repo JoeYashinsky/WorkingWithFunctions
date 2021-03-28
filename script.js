f5a33d8cc9d08fe8c473bc139e4661761b3baf13
Your Name <joey.yashinsky@gmail.com>
2021-03-28 14:01:15 -0400

    poll object has a registwerNewAnswer function that will prompt the user to enter a selection as to what their favorite coding language is

--- script.js
+++ script.js
@@ -252,4 +252,7 @@ document
         question: 'What is your favorite programming language?',
         options: ['0: JavaScript', '1: Python', '2: C#', '3: C++'],
         answers: new Array(4).fill(0),
+        registerNewAnswer() {
+            const answer = Number(prompt(`${this.question}\n${this.options.join('\n')}\n(Write option number)`));
+        },
     };
