37d294bbbdf5574f0929e891b1b2b0dd6eb3451f
Your Name <joey.yashinsky@gmail.com>
2021-04-01 09:30:43 -0400

    added an eventListener so when body of page is clicked, the h1 (header) goes from red to blue

--- script.js
+++ script.js
@@ -374,4 +374,8 @@ boardPassengers(180, 3);
 (function () {
     const header = document.querySelector('h1');
     header.style.color = 'red';
+
+    document.querySelector('body').addEventListener('click', function () {
+        header.style.color = 'blue';
+    });
 })();
